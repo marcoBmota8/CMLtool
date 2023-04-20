3b78da9e11f1596e37e981fec103b34a7fd417e8
Marco Barbero Mota <marco.barbero.mota@vanderbilt.edu>
2023-04-20 10:06:52 -0500

    deleted:    CML_tool/Chain_model_leaky.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
