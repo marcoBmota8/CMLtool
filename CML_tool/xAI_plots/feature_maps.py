7563ed527cf8d34f435324ddb470da43b64f0e0e
Marco Barbero Mota <marco.barbero.mota@vanderbilt.edu>
2025-01-16 13:32:54 -0600

    changes on sources names (in progress)

--- CML_tool/xAI_plots/feature_maps.py
+++ CML_tool/xAI_plots/feature_maps.py
@@ -6,7 +6,7 @@ SLE_2021 = {
         "S-1588": "SLE w/ high DNA double strand Ab",
         "S-1683": "Toxic maculopathy coded in autoimmune patients",
         "S-1378": "Cardiolipin IgG \& IgM Ab", 
-        "S-999" : "Elevated beta-2 \& cadiolipin IgG Ab",
+        "S-999" : "Elevated $\beta-2$ \& cadiolipin IgG Ab",
         "S-1581": "SLE treated w/ HCQ",
         "S-295" : "General injuries",
         "S-788" : "Rheumatoid arthritis",
@@ -19,60 +19,60 @@ SLE_2021 = {
         "S-242" : "High-sensitivity C-reactive protein",
         "S-268" : "Non-tuberculosis mycobacterial pneumonia", # complication of immunosuppression
         'S-1364': 'Vascular insufficiency of the intestine',
-        "S-1897" : "Autoimmune inflammation treated w/ Prednisone", # Check
-        "S-1539" : "Collagen disease \& muscle disorder", # check
-        "S-546" : "Aggressive \& chronic lupus nephritis", # 
-        "S-928" : "Autoimmune hepatitis", # Check 
+        "S-1897" : "Autoimmune inflammation treated w/ Prednisone",
+        "S-1539" : "Collagen disease, muscle disorder \& SLE", 
+        "S-546" : "Aggressive \& chronic lupus nephritis", 
+        "S-928" : "Autoimmune hepatitis",
         "S-835" : "Cutaneous lupus", # Lupus Erythematosus (SNOMED: 200936003 maps to L93, L93.2 & 695.4) -> L ICD10 family is diseases of the skin 
         # cutaneous lupus were considered negatives and still predictive as a positive source.
-        "S-1544" : "Elevated SS-A/Ro Ab", # Check
+        "S-1544" : "Elevated SS-A/Ro Ab", 
         "S-1417" : "Dizziness \& vertigo",
-        "S-031" : "SLE-MCTD overlap syndrome",
+        "S-031" : "Mixed connective tissue disease",
         "S-1135" : "Discoid lupus w/ glomerunephritis", 
         "S-080" : "Early CKD",
         "S-377" : "Aseptic necrosis of bone", # Corticosteroids cause this
         "S-385" : "Raynaud's",
         "S-353" : "Pregnancy complication in group B streptococcus",
-        "S-1114" : "Head injury",
+        "S-1114" : "Head injury (contusion)",
         "S-373" : "Cervical radiculopathy",
         "S-563" : "Elevated IgG, IgA, and erythrocyte sedimentation rate",
         "S-1252" : "Tinea corporis", # Speculation is misDx of some cutaneous lupus
-        "S-1192" : "Elevated beta-2 \& cadiolipin IgM Ab",
-        "S-717" : "APS",
-        "S-1576" : "Systemic sclerosis", # check, do all SS pats get Raynaud’s?/ SS was a Dx excluded from the definitins of SLE and still predictive
+        "S-1192" : "Elevated $\beta-2$ \& cadiolipin IgM Ab",
+        "S-717" : "Antiphospholipid syndrome",
+        "S-1576" : "Systemic sclerosis",# SS was a Dx excluded from the definitins of SLE and still predictive
         "S-1300" : "Elevated specific gravity \& pH of Urine", # Is usually not looked at, unremarkable
         "S-766" : "Diazepan treatment",
         "S-100" : "Late-stage pregnancy complication",
         "S-1853" : "Benign prostatic hyperplasia w/ outflow obstruction", # check
         "S-288" : "Specific visual field defect", # Complicated, might be from the eye exam 
-        "S-1738" : "Alopecia", # Check
-        "S-1717" : "Hematuria", # check 
-        "S-1924" : "Elevated Antithrombin",
+        "S-1738" : "Alopecia, female tendency",
+        "S-1717" : "Hematuria",
+        "S-1924" : "Elevated Antithrombin in blood coagulation disorder", 
         "S-657" : "Mycophenolate mofetil in kidney transplant",
-        "S-1678" : "Undifferentiated connective tissue disease", #check what is different of this and S-031
-        "S-510" : "Second sight phenomenon", # check
-        "S-813" : "Elevated CSF IgG Ab from blood-brain barrier impairment", # check / Lupus cerebritis among other things such as MS
-        "S-658" : "Diplopia", # check
+        "S-1678" : "Undifferentiated connective tissue disease",
+        "S-510" : "Refractive errors \& cataracts", # check
+        "S-813" : "Elevated CSF IgG Ab from blood-brain barrier impairment", # Ask Mike and Vivian / Lupus cerebritis among other things such as MS
+        "S-658" : "Diplopia",
         "S-1238" : "Elevated immature granulocytes", # check
-        "S-1494" : "Sjögren's syndrome", # check -> Compare with the other one with KS
+        "S-1494" : "Sjögren's syndrome",
         "S-539" : "Elevated nuclear anti-SCL-70 Ab", 
         "S-827" : "Elevated anti-Smith Ab", 
-        "S-316" : "$\mathrm{FeSO_4}$ treatment for iron deficiency anemia", # check
-        "S-1348" : "CKD complications", # check
-        "S-1371" : "Degenerative joint disease", # check
-        "S-1413" : "Dermatomyositis", # check -> This Dx was excluded from the SLE definition
+        "S-316" : "$\mathrm{FeSO_4}$ supplementation in anemia",
+        "S-1348" : "Advanced (stage 3) CKD", # check (ask mike) 
+        "S-1371" : "Secondary osteoarthritis", # check
+        "S-1413" : "Dermatomyositis", #-> Excluded in SLE definition so partial matches only 
         "S-1682" : "Elevated SS-A/Ro \& SS-B/La Ab", 
         "S-1062" : "Lumbar spondylosis",
         "S-735" : "Elevated creatinine in urine",
-        "S-237" : "Pain in right knee", # check
+        "S-237" : "Pain in right knee", 
         "S-473" : "High ANA billing code", # check -> Why is there a code for this???
         "S-559" : "Elevated thyroglobulin", 
         "S-980" : "Gabapentin treatment", 
         "S-1936" : "Difficulty walking from lower body weakness", 
-        "S-551" : "Elevated ANA titer result", # Difference with S-473
+        "S-551" : "Elevated ANA titer result", # check Difference with S-473
         "S-448" : "Contact dermatitis",
         "S-1279" : "Elevated erythrocyte sedimentation rate",
-        "S-770" : "Elevated metamyelocytes", # Inicate inflamation
+        "S-770" : "Elevated metamyelocytes", # Indicate inflamation
         "S-1808": "Wrist joint pain",
         "S-069": "Sleep apnea",
         "S-384": "Skin sensation disturbance",
