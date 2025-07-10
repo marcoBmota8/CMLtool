8a9b63fcf4673c23995791b737b707cf1da7e470
marcoBmota8 <marcobarberomota@gmail.com>
2025-07-10 10:12:51 +0200

    Major update after better understanding of TreeExplainer SHAP math

--- CML_tool/ShapUtils.py
+++ CML_tool/ShapUtils.py
@@ -6,14 +6,13 @@ from joblib import Parallel, delayed
 import numpy as np
 from tqdm import tqdm
 
-from shap.maskers import Independent, Partition 
+from shap.maskers import Independent, Partition, Impute
 from shap.explainers import Linear, Exact, Tree, GPUTree
 from shap.links import logit, identity
 
 from CML_tool.ML_Utils import compute_empirical_ci, bootstrap_matrix
 
 
-# %% 
 def calculate_shap_values(
         model,
         background_data,
@@ -22,9 +21,12 @@ def calculate_shap_values(
         pretrained = False,
         explainer_type = None,
         link_function = None,
-        feature_perturbation = 'interventional',
+        feature_perturbation = 'interventional_independent',
         n_samples = 1000,
-        max_samples = 1000
+        max_samples = 1000,
+        retrieve_explainer = True,
+        retrieve_interactions = False
+        
         ):
     '''
     Compute Shapley values using some of the model-agnostic and model-specific approaches 
@@ -35,8 +37,7 @@ def calculate_shap_values(
         -model: model instance (sklearn object) or tuple (coefficients, intercept) for linear models (this requires
         that explainer_type = 'linear').
         -background_data: training data used to compute its mean and covariance which
-        in turn are used to compute conditional expectations (either observational or 
-            interventional). -> numpy.array 
+        in turn are used to compute conditional expectations -> numpy.array 
         -training_outcome: labels or target values for the training instances. -> numpy.array
         -test_data: Data for which predictions shapley values are calculated. -> numpy.array
         -pretrained: Boolean indicating whether the passed model object is already trained on the
@@ -75,9 +76,14 @@ def calculate_shap_values(
               are correlated with (at least one) that are. This option uses the Independent masker.
 
         -n_samples: Only useful for feature_perturbation = 'observational'. Number of samples to use when estimating
-        the transformation matrix used to account for feature correlations. (Default:1000) -> int
+            the transformation matrix used to account for feature correlations. (Default:1000) -> int
         -max_samples: The maximum number of samples to use from the passed background data in the independent masker. 
-        (Default:1000) -> int
+            (Default:1000) -> int
+        -retrieve_explainer: Whether to return the explainer object used to compute the Shapley values.
+            (Default: True) -> bool
+        -retrieve_interactions: Whether to return the interactions matrix instead of shapley values.
+            This is only possible for TreeExplainer and GPUTreeExplainer.
+            (Default: False) -> bool
         
     Returns:
         -Shapley values as a numpy array of the same shape as test_data.
@@ -114,24 +120,27 @@ def calculate_shap_values(
                         Check that the model object you are passing is correct.")
     
     # Define the masker based on the feature perturbation
-    if feature_perturbation == 'interventional':
+    if feature_perturbation == 'interventional_independent':
 
         masker = Independent(
             data = background_data,
             max_samples = max_samples
         )
         
-    elif feature_perturbation == 'observational':
+    elif feature_perturbation == 'interventional_correlation':
 
         masker = Partition(
             data = background_data,
             clustering = 'correlation',
             max_samples = max_samples
         )
-        #TODO consider including masker Impute when missing data exists(?)
-
+    elif feature_perturbation == 'observational':
+        masker = Impute(
+            data = background_data
+        )
+        
     else:
-        raise ValueError('Invalid option. Choose between "interventional" or "observational".')
+        raise ValueError(f'Invalid option {feature_perturbation}. Choose between "interventional_independent", "interventional_correlation" or "observational".')
 
     # define the explainer
     # TODO implement the rest of the explainers (KernelSHAP, etc)
@@ -139,7 +148,7 @@ def calculate_shap_values(
 
         explainer = Linear(
             model = model,
-            masker = masker,
+            masker = masker if 'interventional' in feature_perturbation else masker,
             feature_perturbation = "correlation_dependent" if feature_perturbation == 'observational' else feature_perturbation,
             link = link_function_func,
             nsamples = n_samples,
@@ -200,11 +209,20 @@ def calculate_shap_values(
                           exactly for Tree SHAP values.
                           """, UserWarning)      
                       
-        if feature_perturbation=="interventional":
+        if "interventional" in feature_perturbation:
+            if retrieve_interactions:
+                raise NotImplementedError(""" 
+                                          TreeExplainer Shapley interactions can only be computed using the `observational`
+                                          feature perturbation (i.e. 'tree_path_dependent') which estimates conditional expectations
+                                          based on the trained model strcuture and disregards passed background data.
+                                          """
+                                          )
             if (model_type == 'classifier') & (link_function == 'logit') :
-                raise NotImplementedError ("""Log-odds (logit link) are not supported unless it is the \'raw\' output 
+                raise NotImplementedError ("""
+                                           Log-odds (logit link) are not supported unless it is the \'raw\' output 
                                         of the individual decission trees as in XGBoostClassifier.
-                                        """) #TODO: We want Shapley values expressed in log-odds in this case
+                                        """
+                                        ) #TODO: We want Shapley values expressed in log-odds in this case
             elif (model_type == 'classifier') & (link_function == 'identity'):
                 model_output = 'probability'
             elif model_type == 'regressor':
@@ -215,35 +233,47 @@ def calculate_shap_values(
         if explainer_type == 'tree':
             explainer = Tree(
                 model=model,
-                data=background_data if feature_perturbation == 'interventional' else None,
-                masker=masker,
-                feature_perturbation=feature_perturbation,
+                data=background_data if 'interventional' in feature_perturbation else None, 
+                feature_perturbation='interventional' if 'interventional' in feature_perturbation else 'tree_path_dependent',
                 model_output=model_output,
                 silent=True
             )
         elif explainer_type == 'treeGPU':
             explainer = GPUTree(
                 model=model,
-                data=background_data if feature_perturbation == 'interventional' else None,
-                masker=masker,
-                feature_perturbation=feature_perturbation,
+                data=background_data if 'interventional' in feature_perturbation else None,
+                feature_perturbation='interventional' if 'interventional' in feature_perturbation else 'tree_path_dependent',
                 model_output=model_output,
                 silent=True
             )
             
         warnings.warn(f"WARNING: Provided Shapley values are in {explainer.model.tree_output} units.")
         
-        # Compute Shapley values
-        shap_values = np.array(explainer.shap_values(test_data))[1,...]
-        
-        # Return the shapley values for positive class in binary classification problems
-
+        if retrieve_interactions:
+            shap_interactions = np.array(explainer.shap_interaction_values(test_data))[1,...] # interactions for positive class in binary classification problems
+            warnings.warn(f"""WARNING: 
+                          TreeExplainer Shapley interaction values can only be computed using the observational feature perturbation (conditional expectations).
+                          Hence, in general, the sum across features wont add up to the shapley values computed using the interventional feature perturbation (marginal expectactions),
+                          but they will if computed using the same feature perturbation (expectation)
+                """, UserWarning)   
+            if retrieve_explainer:
+                return shap_interactions, explainer
+            else:
+                return shap_interactions
+        else:
+            shap_values = np.array(explainer.shap_values(test_data))[1,...] # Return the shapley values for positive class in binary classification problems
+            if retrieve_explainer:
+                return shap_values, explainer
+            else:
+                return shap_values
     else:
         raise ValueError(f'Explainer_type: {explainer_type} currently not supported.')
-
-    #compute and return shapley values
-        
-    return shap_values
+    
+    
+    if retrieve_explainer:
+        return shap_values, explainer
+    else:   
+        return shap_values
 
 def CI_shap(
         model,
@@ -257,13 +287,13 @@ def CI_shap(
         alpha = 0.05,
         explainer_type = None,
         link_function = 'identity',
-        feature_perturbation = 'interventional',
+        feature_perturbation = 'interventional_independent',
         n_samples = 1000,
         max_samples = 1000,
         ci_type=None,
         return_samples=False,
         return_mav=True,
-        
+        retrieve_interactions=False
         ):
     '''
     Compute empirical variability and confidence intervals of Shapley values. 
@@ -273,8 +303,7 @@ def CI_shap(
     -Args:
         -model: model instance -> sklearn object
          -background_data: training data used to compute its mean and covariance which
-        in turn are used to compute conditional expectations (either observational or 
-            interventional). -> numpy.array 
+        in turn are used to compute conditional expectations -> numpy.array 
         -training_outcome: labels or target values for the training instances. -> numpy.array
         -test_data: Data for which predictions shapley values are calculated. -> numpy.array
         -test_outcome: labels or target values for the test instances. -> numpy.array
@@ -291,14 +320,15 @@ def CI_shap(
             'identity' (no-op link function, for binary classification this keeps Shapley values as probability) or 
             'logit' (with binary classification models this option expresses each feature Shapley value as log-odds) 
             (Default: 'identity') -> str
-        -feature_perturbation: 'interventional' or 'observational'. For explanation see calculate_shap_values docstring.
-            (Default: 'interventional') -> str
+        -feature_perturbation: 'interventional_independent', 'interventional_correlation' or 'observational'. For explanation see calculate_shap_values docstring.
+            (Default: 'interventional_independent') -> str
         -n_samples: Only useful for feature_perturbation = 'observational'. Number of samples to use when estimating the transformation matrix used 
             to account for feature correlations. (Default:1000) -> int
         -max_samples: The maximum number of samples to use from the passed background data in the independent masker. (Default:1000) -> int
         -ci_type: What confidence interval to compute from the empirical distribution: `pivot` or `quantile` based. Default is to not return neither (Default: None)
         -return_samples: Whether or not to return the full samples matrix.(Default: False).
         -return_mav: Whather or not to return mean absolute value shap values across the set instances, e.g. patients. (Default: True)
+        -retrieve_interactions: Whether to return the interactions matrix instead of shapley values. Only possible for explainer_type='tree'.
         
     -Returns:
         - point estimate (np.array): mean SHAP values for each feature.
@@ -315,21 +345,24 @@ def CI_shap(
     assert isinstance(test_data, np.ndarray), '`test_data` must be a numpy array.'
     
     assert np.array_equal(test_outcomes, test_outcomes.astype(bool).astype(float)), '`test_outcomes` array is not binary.'
+    
 
-    if (ci_type is not None) or (randomness_distortion == 'bootstrapping_test_set'):
-        # Calculate point estimates Shapley values on test data
-        point_estimates = calculate_shap_values(
-                model = model,
-                background_data = background_data,
-                training_outcome = training_outcome,
-                pretrained=False,
-                test_data=test_data,
-                explainer_type=explainer_type,
-                link_function=link_function,
-                feature_perturbation=feature_perturbation,
-                n_samples=n_samples,
-                max_samples=max_samples
-                )
+
+    # Calculate point estimates Shapley values on test data
+    point_estimates, explainer = calculate_shap_values(
+            model = model,
+            background_data = background_data,
+            training_outcome = training_outcome,
+            pretrained=False,
+            test_data=test_data,
+            explainer_type=explainer_type,
+            link_function=link_function,
+            feature_perturbation=feature_perturbation,
+            n_samples=n_samples,
+            max_samples=max_samples,
+            retrieve_explainer=True,
+            retrieve_interactions=retrieve_interactions
+            )
     
     if randomness_distortion == 'train_data_bootstrapping':
         # Estimate confidence intervals through Monte Carlo sampling via bootstrapped samples of the training dataset
@@ -343,7 +376,9 @@ def CI_shap(
             link_function=link_function,
             feature_perturbation=feature_perturbation,
             n_samples=n_samples,
-            max_samples=max_samples
+            max_samples=max_samples,
+            retrieve_explainer=False,
+            retrieve_interactions=retrieve_interactions
             ) for idx in tqdm([
                 np.random.choice(background_data.shape[0], size=background_data.shape[0], replace=True) for _ in range(MC_repeats)
                 ])
@@ -362,7 +397,9 @@ def CI_shap(
             link_function=link_function,
             feature_perturbation=feature_perturbation,
             n_samples=n_samples,
-            max_samples=max_samples
+            max_samples=max_samples,
+            retrieve_explainer=False,
+            retrieve_interactions=retrieve_interactions
             ) for seed in tqdm(np.random.choice(range(MC_repeats), size=MC_repeats, replace=False))
             )
         # Transform samples into an array
@@ -389,24 +426,28 @@ def CI_shap(
     if return_mav:
         return {
             'point_estimates' : point_estimates,
+            'explainer': explainer,
             'shaps': np.mean(abs(shap_values_samples), axis=0).T
         }
     elif return_samples:
         if ci_type is None:
             return {
             'shap_samples': shap_values_samples,
+            'explainer': explainer,
             'point_estimates': point_estimates
         }
         else: 
             return {
                 'shaps_samples':shap_values_samples,
                 'shaps_point_estimates':point_estimates,
+                'explainer': explainer,
                 'ci_lower_bounds':np.array(lower_bounds),
                 'ci_upper_bounds':np.array(upper_bounds)
                 }
     else:
-        logging.info('Neither Shapley sampels matrix nor mean absoliute values were requested. Thus defaulting to returning the full samples matrix and point estimates...')
+        logging.info('Neither Shapley samples matrix nor mean absoliute values were requested. Thus defaulting to returning the full samples matrix, explainer and point estimates...')
         return {
             'shap_samples': shap_values_samples,
-            'point_estimates': point_estimates
+            'point_estimates': point_estimates,
+            'explainer': explainer
         }
